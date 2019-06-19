183507cb66b651ed729a3e3271722bcedf48d858
Sarah <sarah@againstgrav.com>
2019-06-18 23:50:42 -0700

    Updated EnemySegment to use states

--- Assets/Scripts/EnemySegment.cs
+++ Assets/Scripts/EnemySegment.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using TMPro;
@@ -6,6 +7,20 @@ using UnityEngine;
 
 public class EnemySegment : MonoBehaviour
 {
+    #region Enums
+
+    // TODO: Might want to convert this into a proper state machine if stuff gets more complicated
+    public enum EnemySegmentState
+    {
+        Disabled = 0,       // Not in play
+        Inactive = 1,       // Has targetString but can't start typing
+        Active = 2,         // Can start typing
+        Completed = 3,      // Finished typing
+        Destroyed = 4       // Mouse has already right clicked it
+    }
+
+    #endregion
+
     #region Fields
 
     [SerializeField]
@@ -60,8 +75,26 @@ public class EnemySegment : MonoBehaviour
         }
     }
 
-    public bool IsComplete { get; private set; } = false;
-    public bool IsDestroyed { get; private set; } = false;
+    private EnemySegmentState _currentState = EnemySegmentState.Disabled;
+    public EnemySegmentState CurrentState
+    {
+        get { return _currentState; }
+        set
+        {
+            if(value != _currentState)
+            {
+                _currentState = value;
+                UpdateVisuals();
+                EnemySegmentStateChangeEvent?.Invoke(this);
+            }
+        }
+    }
+
+    #endregion
+
+    #region Events
+
+    public event Action<EnemySegment> EnemySegmentStateChangeEvent;
 
     #endregion
 
@@ -71,14 +104,12 @@ public class EnemySegment : MonoBehaviour
     {
         markedColorHex = ColorUtility.ToHtmlStringRGB(markedColor);
         unmarkedColorHex = ColorUtility.ToHtmlStringRGB(unmarkedColor);
-
-        SetTargetString("TESTING");
     }
 
     private void OnMouseOver()
     {
         // Right click
-        if (IsComplete && !IsDestroyed && Input.GetMouseButtonDown(1))
+        if (CurrentState == EnemySegmentState.Completed && Input.GetMouseButtonDown(1))
         {
             DestroySegment();
         }
@@ -88,12 +119,14 @@ public class EnemySegment : MonoBehaviour
 
     public void SetTargetString(string newTarget)
     {
-        IsComplete = false;
-        IsDestroyed = false;
         firstUnmarkedCharIndex = 0;
         TargetString = newTarget;
+        CurrentState = EnemySegmentState.Inactive;
+    }
 
-        UpdateVisuals();
+    public void ActivateSegment()
+    {
+        CurrentState = EnemySegmentState.Active;
     }
 
     private IEnumerator UpdateColliderSize()
@@ -106,47 +139,56 @@ public class EnemySegment : MonoBehaviour
 
     public void TryMarkChar(char charToTry)
     {
-        if (!IsComplete && FirstUnmarkedChar == charToTry)
+        // TODO: Handle spaces
+        if (CurrentState == EnemySegmentState.Active && FirstUnmarkedChar == charToTry)
         {
             firstUnmarkedCharIndex++;
             if (firstUnmarkedCharIndex == TargetString.Length)
             {
-                IsComplete = true;
+                CurrentState = EnemySegmentState.Completed;
             }
 
             UpdateVisuals();
-        }
-        else
-        {
-            // TODO: Bad stuff happens
-            Debug.LogError("BAD STUFF HAPPENS!");
         }
     }
 
     private void UpdateVisuals()
     {
-        if (IsDestroyed)
+        switch (CurrentState)
         {
-            // TODO: Something
-        }
-        else if (IsComplete)
-        {
-            // TODO: More visual feedback
-            text.SetText($"<color=#{markedColorHex}>{TargetString}");
-        }
-        else
-        {
-            // No char has been marked
-            if (firstUnmarkedCharIndex == 0)
-            {
-                text.SetText($"<color=#{unmarkedColorHex}><u>{TargetString[0]}</u>{TargetString.Substring(1)}");
-            }
-            else
-            {
-                text.SetText($"<color=#{markedColorHex}>{TargetString.Substring(0, firstUnmarkedCharIndex)}" +   // Marked
-                             $"<color=#{unmarkedColorHex}><u>{TargetString[firstUnmarkedCharIndex]}</u>" +       // First unmarked char is underlined
-                             $"{TargetString.Substring(firstUnmarkedCharIndex+1)}");                             // Remaining unmarked
-            }
+            case EnemySegmentState.Disabled:
+                gameObject.SetActive(false);
+                break;
+
+            case EnemySegmentState.Inactive:
+                text.SetText($"<color=#{unmarkedColorHex}>{TargetString}");
+                gameObject.SetActive(true);
+                break;
+
+            case EnemySegmentState.Active:
+                // No char has been marked
+                if (firstUnmarkedCharIndex == 0)
+                {
+                    text.SetText($"<color=#{unmarkedColorHex}><u>{TargetString[0]}</u>{TargetString.Substring(1)}");
+                }
+                else
+                {
+                    text.SetText($"<color=#{markedColorHex}>{TargetString.Substring(0, firstUnmarkedCharIndex)}" +   // Marked
+                                 $"<color=#{unmarkedColorHex}><u>{TargetString[firstUnmarkedCharIndex]}</u>" +       // First unmarked char is underlined
+                                 $"{TargetString.Substring(firstUnmarkedCharIndex + 1)}");                             // Remaining unmarked
+                }
+                break;
+
+            case EnemySegmentState.Completed:
+                // TODO: More visual feedback
+                text.SetText($"<color=#{markedColorHex}>{TargetString}");
+
+                break;
+
+            case EnemySegmentState.Destroyed:
+                // TODO: Something
+                break;
+
         }
     }
 
@@ -155,6 +197,6 @@ public class EnemySegment : MonoBehaviour
         Debug.LogError($"DESTROYED {this.name}!", gameObject);
         // TODO: What exactly does destroying this entail?
 
-        IsDestroyed = true;
+        CurrentState = EnemySegmentState.Destroyed;
     }
 }
