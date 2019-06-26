c00a0b1bf350b3328a306bfcc23beab1bb9027b7
AntSharK <great_ant@hotmail.com>
2019-06-26 01:38:18 -0700

    Hehe if you press SPACEbar your cursor becomes a SPACEship

--- Assets/Scripts/Reticle.cs
+++ Assets/Scripts/Reticle.cs
@@ -5,13 +5,46 @@ using UnityEngine;
 public class Reticle : MonoBehaviour
 {
     [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float forceMultiplier;
+    [SerializeField]
     private float maxSpeed;
+
+    // TODO: Prototype for unlocking cursor
+    private bool cursorLocked = false;
 
     private void Update()
     {
-        // Clamp it to ensure the position is always within the screen
-        //Vector2 clampedMousePos = new Vector2(Mathf.Clamp(Input.mousePosition.x, 0, Screen.width), Mathf.Clamp(Input.mousePosition.y, 0, Screen.height));
-        var targetPos = Utils.MainCam.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = Vector2.Lerp(transform.position, targetPos, Time.deltaTime * maxSpeed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cursorLocked = !cursorLocked;
+            if (cursorLocked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+
+        if (cursorLocked)
+        {
+            // Clamp it to ensure the position is always within the screen
+            var pos = Utils.MainCam.WorldToViewportPoint(rb.position);
+            pos.x = Mathf.Clamp01(pos.x);
+            pos.y = Mathf.Clamp01(pos.y);
+            rb.transform.position = Utils.MainCam.ViewportToWorldPoint(pos);
+
+            var forceDirection = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            rb.AddForce(forceDirection * forceMultiplier);
+        }
+        else
+        {
+            var targetPos = Utils.MainCam.ScreenToWorldPoint(Input.mousePosition);
+            rb.transform.position = Vector2.Lerp(transform.position, targetPos, Time.deltaTime * maxSpeed);
+        }
     }
 }
